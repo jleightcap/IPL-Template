0532810ce1359e1abebd789315c5811036922994
jleightcap <jleightcap@gmail.com>
2019-06-13 22:40:01 -0400

    Combine multiple template repositories

--- Code/RandCode.py
+++ Code/RandCode.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
+def fib(n):
    if n <= 1:
-       return n
+       return 1
    else:
-       return(fibonacci(n-1) + fibonacci(n-2))
+       return fib(n-1) + fib(n-2)
---
+++ Code/RandCode.rkt
@@ -0,0 +1,3 @@
+(define (fib n)
+  (cond [(<= n 1) n]
+        [else (+ (fib (- n 1)) (fib (- n 2)))]))
