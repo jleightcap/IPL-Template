9d86a1411611a1108b7baabee66356a34b778210
jleightcap <jleightcap@protonmail.com>
2021-01-18 19:37:30 -0500

    docker in shell script

---
+++ latexmk.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# docker run -dt latex
+DOCK="$(docker ps | grep "latex" |  awk '{ print $1 }')"
+
+if [ -z "${DOCK}" ]; then
+    echo "starting detatched latex container..."
+    docker run -dt --mount src=/tmp/latex,target=/data,type=bind --name latex-test latex
+    docker-machine ssh docker1 "mkdir -p /tmp/latex"
+fi
+
+docker-machine scp -r -d -q . docker1:/tmp/latex
+
+docker exec "$DOCK" latexmk "$@"
+
+docker-machine scp -r -d -q docker1:/tmp/latex/ .
